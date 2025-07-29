fcdd0173589a4f0de8427d692d8007bd7d595aca
Shivedra123 <shivendrasaket2580@gmail.com>
2025-07-29 18:31:55 +0530

    express second code

--- express_js/index.js
+++ express_js/index.js
@@ -1,27 +1,21 @@
+
 const express = require("express");
+const app = express(); 
 
-const app = express();
-
+// Middleware 
 app.use((req, res, next) => {
   console.log("Middleware chal raha hai:", req.method, req.url);
-  next();
-});
-
-app.get("/", (req, res) => {
-  res.send("Home Page par ho tum!");
-});
-
-app.get("/about", (req, res) => {
-  res.send("About Page par ho tum!");
+  next(); 
 });
 
 
-//  Start server
+app.use((req, res, next) => {
+  console.log("Code successfully run:", req.url, req.method);
+  res.send("<p>Hey, I am Shivendra and this is my second Express code!</p>");
+});
+
+
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`✅ Server is running at http://localhost:${port}`);
 });
-
-
-//end 
-console.log("shivendra mauhariyaa")
--- express_js/server.js
+++ express_js/server.js
@@ -1,20 +1,20 @@
-// server.js
-const express = require('express');
-const app = express();
-const noteRoutes = require('./routes/notes');
-const PORT = 3000;
+// // server.js
+// const express = require('express');
+// const app = express();
+// const noteRoutes = require('./routes/notes');
+// const PORT = 3000;
 
-// middleware
-app.use(express.json());
+// // middleware
+// app.use(express.json());
 
-// route use
-app.use('/api/notes', noteRoutes);
+// // route use
+// app.use('/api/notes', noteRoutes);
 
-// home route
-app.get('/', (req, res) => {
-    res.send('Welcome to Note Keeper API');
-});
+// // home route
+// app.get('/', (req, res) => {
+//     res.send('Welcome to Note Keeper API');
+// });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on http://localhost:${PORT}`);
+// });
