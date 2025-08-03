877caa46f111efbd62034a93eb9da3fc57de11aa
Shivedra123 <shivendrasaket2580@gmail.com>
2025-08-03 14:11:37 +0530

    res

--- express_js/index.js
+++ express_js/index.js
@@ -11,11 +11,11 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log("Code successfully run yes:", req.url, req.method);
-  res.send("<p>Hey I am Shivendra  this is Express code......</p>");
+  res.send("<p>Hey I a am. mauhariya....</p>");
 });
 
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`✅ Server is running at http://localhost:${port}`);
+  console.log(` Server is running at http://localhost:${port}`);
 });
