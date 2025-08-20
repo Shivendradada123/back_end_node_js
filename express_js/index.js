8aa3df7db736c279c45401defe24e7783bd1ef37
Shivedra123 <shivendrasaket2580@gmail.com>
2025-08-20 13:49:35 +0530

    a

--- express_js/index.js
+++ express_js/index.js
@@ -11,7 +11,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log("Code successfully run yes:", req.url, req.method);
-  res.send("<p> hey i am shivendra and i am excellance </p>");
+  res.send("<p> hey i am shivendra and i am excellance the jathi school </p>");
 });
 
 
@@ -25,3 +25,4 @@ app.listen(port, () => {
 
 
 
+con
